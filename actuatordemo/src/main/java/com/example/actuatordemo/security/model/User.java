01acf34779f21c293b34ba7ece947d80f57d95bd
Arin <Arin@Arin-PC.fios-router.home>
2018-03-11 22:23:32 -0400

    Added a New Model Class

---
+++ actuatordemo/src/main/java/com/example/actuatordemo/security/model/User.java
@@ -0,0 +1,43 @@
+package com.example.actuatordemo.security.model;
+
+import java.io.Serializable;
+import java.util.Set;
+
+public class User implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2585357112568793048L;
+	private String userName;
+	private String userPassword;
+	private Set<String> userRole;
+	
+	public User() {
+		super();
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public Set<String> getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(Set<String> userRole) {
+		this.userRole = userRole;
+	}
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", userPassword=" + userPassword + ", userRole=" + userRole + "]";
+	}
+	
+}
