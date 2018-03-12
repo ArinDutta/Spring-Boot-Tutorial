32b55b9f63717afe727631dfb022014be8abcf01
Arin <Arin@Arin-PC.fios-router.home>
2018-03-11 23:54:45 -0400

    Added nCustom Security Configuration

--- actuatordemo/src/main/java/com/example/actuatordemo/security/CustomSecurityConfig.java
+++ actuatordemo/src/main/java/com/example/actuatordemo/security/CustomSecurityConfig.java
@@ -1,5 +1,20 @@
 package com.example.actuatordemo.security;
 
-public class CustomSecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class CustomSecurityConfig  extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin().and()
+			.httpBasic();
+	}
 
 }
